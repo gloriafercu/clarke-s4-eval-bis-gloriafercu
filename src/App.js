47fc37c686fbbbc2c418d09deb9f21dfffe4d59f
gloriafercu <gloriafercu@gmail.com>
2018-02-09 20:03:43 +0100

    create react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '././scss/main.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
