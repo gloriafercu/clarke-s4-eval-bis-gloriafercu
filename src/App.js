5ea293059a9bfbc7ac6f1f3bcf69b594dca8dca5
gloriafercu <gloriafercu@gmail.com>
2018-02-11 22:33:44 +0100

    print repos

--- src/App.js
+++ src/App.js
@@ -5,15 +5,54 @@ import RepoCard from './components/RepoCard';
 import '././scss/main.css';
 
 class App extends React.Component {
-  render() {
-    return (
+	constructor(props) {
+		super(props);
+		this.state = {
+			repositories: []
+		};
+	}
+
+	componentDidMount() {
+		fetch('https://api.github.com/orgs/Adalab/repos')
+		.then(response => response.json())
+		.then(json => {
+			this.setState({
+				repositories: json
+			});
+			// const listRespositories = json;
+			// console.log(json.map (
+			// 	list => list.name
+			// ));
+		});
+	}
+
+	printRepositories() {
+ 		const listRespositories = this.state.repositories;
+		return (
+			<ul className="list__repos">
+				{ listRespositories.map((list, index)=>
+					<li className="repo__card" key = {index}>
+						<RepoCard
+							name = {list.name}
+							url = {list.html_url}
+							description = {list.description}
+							lang = {list.language}
+						/>
+					</li>)
+				}
+			</ul>
+		);
+	}
+
+ render() {
+		return (
 			<div className="App">
 				<header className="header__title">
 					<h1>Repos at Adalab in GitHub</h1>
 				</header>
 				<main className="wrapper">
 					<Search />
-					<RepoCard />
+					{ this.printRepositories() }
 				</main>
 			</div>
     );
